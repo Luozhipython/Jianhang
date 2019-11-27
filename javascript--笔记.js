1092df3fcc5667e0f105beeb6201e88cdf8bec9f
FengJianHang <69952604@qq.com>
2019-11-27 18:42:46 +0800

    no message

---
+++ javascript--笔记.js
@@ -0,0 +1,14 @@
+/*day01
+JavaScript
+JavaScript 是Web的编程语言
+JavaScript 是互联网上最流行的脚本语言
+1 JavaScript是一种轻量级的编程语言
+2 JavaScript是可插入HTML页面的编程代码
+3 JavaScript插入HTML页面后，可由所有的现代浏览器执行
+
+JavaScript: 直接写入HTML输出流
+	document.write("<h1>这是一个标题</h1>");
+	document.write("<p>这是一个段落</p>");
+JavaScript: 对事件的反应
+<button type="button" onclick="alert('欢迎')">点我！</button>
+*/
